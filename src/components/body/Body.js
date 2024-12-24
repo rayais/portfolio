0ff6ab51590fe43e0d8bc02a7eab9401224d7bea
Rayen BM <rayen.ben.mansour@gmail.com>
2024-12-24 15:48:43 +0100

    24122024

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Certif from './Certif/Certif'
+import Projects from './Personal/Projects'
 
 function Body() {
   return (
     <div>
         <Certif/>
-        
+        <Projects/>
     </div>
   )
 }
--- src/components/body/Personal/Projects.jsx
+++ src/components/body/Personal/Projects.jsx
@@ -1,4 +1,4 @@
-import "./certif.css";
+//import "./certif.css";
 
 function Projects() {
   const pros = [
@@ -6,16 +6,24 @@ function Projects() {
       titre: "Carpool web app",
 
       presentation:
-        "",
-      pic: "../../../asse/dsi.png",
+        "LiftShare is a carpooling application built using the MERN stack (MongoDB, Express, React, Node.js) designed to connect drivers and passengers for shared rides. Key features include:"+
+
+"Geonames API Integration: Dynamically loads location names for enhanced user experience."+
+"Cloudinary Integration: Securely handles photo uploads for profile pictures and car images."+
+"Backend: Developed with Express, incorporating robust CORS handling and essential security middleware."+
+"Hosting: Fully deployed on Render, ensuring seamless accessibility",
+      pic: "../../../asse/logolift.png",
       link: "",
     },
     {
       
       titre: "Geoفين",
       presentation:
-        "",
-      pic: "../../../asse/gomycode.png",
+        "GeoFeen started as an educational project initially developed using WordPress, but I decided to rebuild it using React to enhance my front-end development skills. This static website showcases:"+
+"A modern, responsive design powered by React for a seamless user experience."+
+"Custom components and reusable UI elements for scalability and maintainability."+
+"A transition from WordPress to React, reflecting my journey to sharpen my front-end expertise.",
+      pic: "../../../asse/logo1.png",
       link: "",
     },
   ];
@@ -26,7 +34,7 @@ function Projects() {
       <div className="cers">
         <table>
           <tbody>
-            {Pros.map((ce, i) => (
+            {pros.map((ce, i) => (
               <tr key={i}>
                 <td className="certext">
                   <ul>
